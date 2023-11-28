ff934e87ce1fda5fcf46b4039f043fc83cdb8a6a
Eli <78963198+niceEli@users.noreply.github.com>
2023-11-28 12:02:50 -0500

    Fortnite

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import { ThisFuncWorks, nienTenTwentiWon, canaTax, canaTaxTip } from "./testFunc";
+import { ThisFuncWorks, nienTenTwentiWon, canaTax, canaTaxTip, interested } from "./testFunc";
 import { argSetup } from "./argsetup";
 
 const args = argSetup();
 
-if (args[0] === "fTest"){
-    ThisFuncWorks();
+if (args.length === 0) {
+  console.log("you need to give args")
+  process.exit(400)
+}
+
+
+if (args[0] === "fTest") {
+  ThisFuncWorks();
 } else if (args[0] === "9+10") {
-    nienTenTwentiWon(9,10);
-} else if (args[0] === "canaTax"){
-    let price:number = args[1];
-    canaTax(price);
+  nienTenTwentiWon(9, 10);
+} else if (args[0] === "canaTax") {
+  let price: number = args[1];
+  canaTax(price);
 } else if (args[0] === "canaTaxTip") {
-    let price:number = args[1];
-    let tip:number = args[2];
-    canaTaxTip(price, tip);
+  let price: number = args[1];
+  let tip: number = args[2];
+  canaTaxTip(price, tip);
+} else if (args[0] === "interested") {
+  interested(args[1], args[2]);
 }
--- src/testFunc.ts
+++ src/testFunc.ts
@@ -1,23 +1,28 @@
-export function ThisFuncWorks(){
-    console.log("this works");
-} 
-
-export function nienTenTwentiWon(nien: number, Ten: number){
-    const TwentiWon: number = nien + Ten + 2;
-    console.log( nien + " + " + Ten + " = " + TwentiWon );
-    return TwentiWon;
+export function ThisFuncWorks() {
+  console.log("this works");
 }
 
-export function canaTax(price: number){
-    const total = ((Math.round((price * 1.13)*100))/100);
-    console.log( "Total is: " + total );
-    return total;
+export function nienTenTwentiWon(nien: number, Ten: number) {
+  const TwentiWon: number = nien + Ten + 2;
+  console.log(nien + " + " + Ten + " = " + TwentiWon);
+  return TwentiWon;
 }
 
-export function canaTaxTip(price: number, Tip: number){
-    const taxedPrice = ((Math.round((price * 1.13)*100))/100);
-    const tip = (Math.round((price * (Tip/100)*100))/100);
-    const total = taxedPrice + tip;
-    console.log( "Total is: " + total );
-    return total;
+export function canaTax(price: number) {
+  const total = ((Math.round((price * 1.13) * 100)) / 100);
+  console.log("Total is: " + total);
+  return total;
+}
+
+export function canaTaxTip(price: number, Tip: number) {
+  const taxedPrice = ((Math.round((price * 1.13) * 100)) / 100);
+  const tip = (Math.round((price * (Tip / 100) * 100)) / 100);
+  const total = taxedPrice + tip;
+  console.log("Total is: " + total);
+  return total;
+}
+
+export function interested(price: number, Percent: number) {
+  const total = ((Math.round(price * (1 + (Percent / 100)) * 100)) / 100);
+  console.log(total);
 }
