7891907b34ef8d6ef7cfbb47f3328feaded16719
Eli <78963198+niceEli@users.noreply.github.com>
2023-11-28 14:03:08 -0500

    i think its broken but it p cool also im use nvim btw (lazyvim distro)

--- src/argsetup.ts
+++ src/argsetup.ts
@@ -1,5 +1,7 @@
-export function argSetup(){
-    let args:Array<any> = process.argv;
-    args.splice(0, 2);
-    return args;
+export function argSetup() {
+  //this script is poggers and also gives me args from java or c#
+  console.log("Seting Up Arguments");
+  let args: Array<any> = process.argv;
+  args.splice(0, 2);
+  return args;
 }
